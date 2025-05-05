e65e500ead0d187931e603274f7bb5bd17d87e37
victor22efrei <victor.vaures@efrei.net>
2025-05-05 11:34:53 +0200

    la y a l'acceuil faut juste changer les fonction pour appeler le jeu et il y a le truc tuto, faut juste faire l'image du tuto

---
+++ Tuto.py
@@ -0,0 +1,20 @@
+
+import pygame
+def tuto(fenetre,largeur,longueur):
+    image_de_fond = pygame.image.load("fond.png").convert()
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                return
+
+        image_de_fond_redimensionnee = pygame.transform.scale(image_de_fond, (largeur, longueur))
+        fenetre.blit(image_de_fond_redimensionnee, (0, 0))
+        pygame.display.flip()
+        clock.tick(60)
