420049d331b5035aab2b30f4b644200912bbe933
Victor22efrei <victor.vaures@efrei.net>
2025-05-11 20:27:12 +0200

    Update map+deplacement.py

--- map+deplacement.py
+++ map+deplacement.py
@@ -22,7 +22,107 @@ mute_button_image = pygame.transform.scale(mute_button_image, (30, 30))  # Redim
 mute_button_rect = mute_button_image.get_rect()
 mute_button_rect.topright = (screen.get_width() - 10, 10)  # Position en haut à droite avec un décalage de 10 pixels
 
-# Classe Personnage
+#class niveau 1
+class Perso:
+
+    def __init__(self, image_path, start_pos):
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (300, 200))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = start_pos
+        self.y_eau = 850
+        self.speed = 3
+        self.vitesse_y = 0
+        self.en_saut = False
+        self.gravite = 0.086
+        self.live = True
+
+    def deplacer(self, touches, keys, screen_width, screen_height):
+
+
+        #Dans l'eau
+        if self.rect.top > self.y_eau:
+            if keys[touches['left']] and self.rect.left > 0:
+                self.rect.x -= self.speed
+            if keys[touches['right']] and self.rect.right < screen_width:
+                self.rect.x += self.speed
+            if keys[touches['up']]:
+                self.rect.y -= self.speed
+            if keys[touches['down']] and self.rect.bottom < screen_height:
+                self.rect.y += self.speed
+
+
+        #Sortie de l'eau 01
+        elif self.rect.top <= self.y_eau:
+
+            if keys[touches['left']] and self.rect.left > 0:
+                self.rect.x -= self.speed
+            if keys[touches['right']] and self.rect.right < screen_width * 2:  # Limite sur la droite augmentée
+                self.rect.x += self.speed
+
+            if keys[touches['down']] and self.rect.bottom < screen_height:  # Limite en hauteur augmentée
+                self.rect.y += self.speed
+
+            if keys[touches['space']] and not self.en_saut:
+                self.vitesse_y = -11
+                self.en_saut = True
+
+            if self.en_saut:
+                self.vitesse_y += self.gravite
+                self.rect.y += self.vitesse_y
+
+
+                if keys[touches['left']] and self.rect.left > 0:
+                    self.rect.x -= self.speed
+                if keys[touches['right']] and self.rect.right < screen_width * 2:
+                    self.rect.x += self.speed
+
+                #Fin du saut : quand on touche l'eau
+                if self.rect.top >= self.y_eau:
+                    self.rect.top = self.y_eau
+                    self.vitesse_y = 0
+                    self.en_saut = False
+
+
+
+    def dessiner(self, screen, camera_offset_x,camera_offset_y): # permet de suivre le personnage ( avec la caméra )
+        screen.blit(self.image, (self.rect.x - camera_offset_x, self.rect.y - camera_offset_y))
+
+
+
+
+class coin:
+    def __init__(self, position, img):
+        self.pos = (10,10)
+        self.img = pygame.transform.scale(pygame.image.load(img), (50, 50))
+        self.nombre_de_piece=0
+        self.ramassee = False
+        self.rect = self.img.get_rect(topleft=self.pos)
+        self.font = pygame.font.SysFont("None", 36)
+
+    def ajouter_des_pieces (self,nb=1):
+        self.nombre_de_piece+=nb
+
+    def ramasser(self):
+        if not self.ramassee:
+            self.ajouter_des_pieces(1)
+            self.ramassee = True
+
+    def get_pieces (self):
+        return self.nombre_de_piece
+
+    def dessiner_menu(self, screen):
+        if not self.ramassee:
+            screen.blit(self.img, (self.rect.x , self.rect.y) )
+        texte = self.font.render(f"x {self.nombre_de_piece}", True, (255, 255, 255))
+        screen.blit(texte, (self.pos[0] + 60, self.pos[1] + 10))
+
+    def dessiner(self, screen,camera_offset_x,camera_offset_y):
+        if not self.ramassee:
+            screen.blit(self.img, (self.rect.x - camera_offset_x, self.rect.y - camera_offset_y))
+        texte = self.font.render(f"x {self.nombre_de_piece}", True, (255, 255, 255))
+        screen.blit(texte, (self.pos[0] + 60, self.pos[1] + 10))
+# Classe Personnage niveau 2
 class Personnage:
     def __init__(self, image_path, start_pos):
         self.image = pygame.image.load(image_path)
