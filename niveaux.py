e98b49a7c7a633b414e46a3197973e499f9699ec
Victor22efrei <victor.vaures@efrei.net>
2025-05-13 11:08:58 +0200

    Update niveaux.py

--- niveaux.py
+++ niveaux.py
@@ -1,7 +1,10 @@
 import pygame
-from niveau1 import Niveau1
+from niveau11 import Niveau1
 from ecran_fin import fenetre_défaite
 from ecran_fin import fenetre_victoire
+from niveau2 import Niveau2
+
+
 def image(fenetre,largeur,longueur):
 
     # Police et assets.
@@ -33,17 +36,19 @@ def image(fenetre,largeur,longueur):
                         pygame.display.set_caption("shark.io")
 
                     elif pygame.Rect(530, 677, 200, 59).collidepoint(pos):  # Guide
-                        current_screen = "shark.io"
-                        # mettre la fonction pour le premier niveau
-                        print("Lancer le jeu")  # METTRE FONCTION QUI PERMET DE LANCER LE JEU
+                        current_screen = "niveaux_2"
+                        win = Niveau2()
+                        if win == 0:
+                            fenetre_défaite(fenetre, longueur, largeur)
+                        else:
+                            fenetre_victoire(fenetre, longueur, largeur)
+
+                        current_screen = "menu"
+                        pygame.display.set_caption("shark.io")
+
                     elif pygame.Rect(224, 74, 817, 155).collidepoint(pos):  # Start
-                        while running:
-                            for event in pygame.event.get():
-                                if event.type == pygame.QUIT:
-                                    pygame.quit()
-                                    exit()
-                                elif event.type == pygame.MOUSEBUTTONDOWN:
-                                    return
+                        return
+
 
 
         pos_souris = pygame.mouse.get_pos()
@@ -52,7 +57,6 @@ def image(fenetre,largeur,longueur):
         image_de_fond_redimensionnee = pygame.transform.scale(image_de_fond_original, (largeur, longueur))
         #Affichage
         fenetre.blit(image_de_fond_redimensionnee, (0, 0))
-
         # Affichage des coordonnées de la souris
         #text_coord = font.render(f"X:{pos_souris[0]} Y:{pos_souris[1]}", True, (255, 255, 255))
         #fenetre.blit(text_coord, (10, 10))
